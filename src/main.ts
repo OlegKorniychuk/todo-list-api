02b6b40846d1ccb52a77e4488b148a67078d64e2
OlegKorniychuk <kornoleg576@gmail.com>
2026-07-31 16:24:33 +0300

    feat: add request logging with pino

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
 
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
